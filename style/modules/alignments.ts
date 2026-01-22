f4746762911900c46acc46452fcc9016014ab719
DerMixer <mickihde9@gmail.com>
2026-01-22 22:38:42 +0100

    workflow foundation
    - added all images & icons from old projects
    - created fundamental modules for colors & common ui alignments

---
+++ style/modules/alignments.ts
@@ -0,0 +1,31 @@
+export const commonSpacing = {
+    fullCenter: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    xCenter: {
+        display: 'flex',
+        justifyContent: 'center',   
+    },
+    yCenter: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    SA: {
+        display: 'flex',
+        justifyContent: 'space-around',
+    },
+    SB: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    FR: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    FC: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+}
---
+++ style/modules/generalTheme.ts
@@ -0,0 +1,40 @@
+export const colorPalette = {
+    light: {
+        background: {
+            dark: '#f2f2f2',
+            medium: '#e6e6e6',
+            light: '#ffffff'
+        },
+        border: '#cccccc',
+        text: {
+            light: '#1a1a1a',
+            muted: '#666666',
+        },
+    },
+    dark: {
+        background: {
+            dark: '#0d0d0d',
+            medium: '#1a1a1a',
+            light: '#262626'
+        },
+        border: '#666666',
+        text: {
+            light: '#f2f2f2',
+            muted: '#b3b3b3',
+        },
+    },
+    primary: {
+        main: '#8cb3c8',
+        dark: '#6699b3',
+    },
+    feedback: {
+        danger: '#cc6566',
+        warning: '#cccc65',
+        success: '#65cc64',
+        info: '#8c8cd9'
+    }
+}
+
+export const typographySettings = {
+
+}
